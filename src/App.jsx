bc0fe7948a97273ddbfcb4e48d3a8d039a287cc0
Denise Stark <starkwalker88@gmail.com>
2024-04-01 16:13:09 -0700

    finished up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
+import Header from "./components/Header.jsx";
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Not easy" targetTime={5} />
-        <TimerChallenge title="Getting tough" targetTime={10} />
-        <TimerChallenge title="Pros only" targetTime={15} />
+      <Header />
+      <div id="content">
+        <header>
+          <h1>
+            The <em>Almost</em> Final Countdown
+          </h1>
+          <p>Stop the timer once you estimate that time is (almost) up</p>
+        </header>
+        <Player />
+        <div id="challenges">
+          <TimerChallenge title="Easy" targetTime={1} />
+          <TimerChallenge title="Not easy" targetTime={5} />
+          <TimerChallenge title="Getting tough" targetTime={10} />
+          <TimerChallenge title="Pros only" targetTime={15} />
+        </div>
       </div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div id="masthead">
+        <a
+          className="navbar-brand"
+          href="https://dstark88.github.io/portfolio/"
+        >
+          Denise Stark
+        </a>
+        <nav className="navbar">
+          <a href="https://dstark88.github.io/portfolio/" target="blank">
+            Portfolio
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
